3728e83c142e2ba9bc4e0f57317bb64d80c26f44
Your Name <you@example.com>
2017-06-21 01:47:14 +0200

    Amelioration de creer un compte avec des verifications

--- Application IHM/Application IHM/CreeId.vb
+++ Application IHM/Application IHM/CreeId.vb
@@ -1,7 +1,20 @@
 ﻿Public Class CreeId
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("Bienvenu " & TextBoxPseudo.Text)
-        Me.Dispose()
+        'Verification si les champs sont remplis
+        If TextBoxPseudo.Text = "" Or TextBoxMdp.Text = "" Or BoxNom.Text = "" Or BoxPrenom.Text = "" Or BoxAge.Text = "" Then
+            MsgBox("Veuillez rentrer tout les champs")
+        Else
+            MsgBox("Bienvenu " & TextBoxPseudo.Text)
+            Me.Dispose()
+        End If
 
+    End Sub
+
+    Private Sub BoxNom_TextChanged(sender As Object, e As EventArgs) Handles BoxNom.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Dispose()
     End Sub
 End Class
