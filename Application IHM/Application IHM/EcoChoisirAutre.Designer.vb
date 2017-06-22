20eda6e81acce0f3b93569c99b7d97dd3c68a0d2
Your Name <you@example.com>
2017-06-22 03:26:56 +0200

    finition de la fonction Eco manque plus que le design de l'application

--- Application IHM/Application IHM/EcoChoisirAutre.Designer.vb
+++ Application IHM/Application IHM/EcoChoisirAutre.Designer.vb
@@ -22,8 +22,304 @@ Partial Class EcoChoisirAutre
     'Ne la modifiez pas à l'aide de l'éditeur de code.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.LblPrixPorteClef = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.LblPrixGob = New System.Windows.Forms.Label()
+        Me.BtnRetour = New System.Windows.Forms.Button()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.Panel2.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(201, 273)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(37, 17)
+        Me.Label7.TabIndex = 147
+        Me.Label7.Text = "euro"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Items.AddRange(New Object() {"Poids : 2g piéce", "Marque : Gobigoba", "Pays : France", "Bio : NON"})
+        Me.ListBox1.Location = New System.Drawing.Point(17, 139)
+        Me.ListBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(124, 68)
+        Me.ListBox1.TabIndex = 142
+        Me.ListBox1.Visible = False
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(165, 404)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(103, 33)
+        Me.Button5.TabIndex = 141
+        Me.Button5.Text = "Appliquer"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'LblPrixPorteClef
+        '
+        Me.LblPrixPorteClef.AutoSize = True
+        Me.LblPrixPorteClef.Location = New System.Drawing.Point(167, 277)
+        Me.LblPrixPorteClef.Name = "LblPrixPorteClef"
+        Me.LblPrixPorteClef.Size = New System.Drawing.Size(16, 17)
+        Me.LblPrixPorteClef.TabIndex = 139
+        Me.LblPrixPorteClef.Text = "3"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(201, 153)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(37, 17)
+        Me.Label9.TabIndex = 138
+        Me.Label9.Text = "euro"
+        '
+        'LblPrixGob
+        '
+        Me.LblPrixGob.AutoSize = True
+        Me.LblPrixGob.Location = New System.Drawing.Point(162, 152)
+        Me.LblPrixGob.Name = "LblPrixGob"
+        Me.LblPrixGob.Size = New System.Drawing.Size(16, 17)
+        Me.LblPrixGob.TabIndex = 136
+        Me.LblPrixGob.Text = "5"
+        '
+        'BtnRetour
+        '
+        Me.BtnRetour.Location = New System.Drawing.Point(17, 68)
+        Me.BtnRetour.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.BtnRetour.Name = "BtnRetour"
+        Me.BtnRetour.Size = New System.Drawing.Size(75, 23)
+        Me.BtnRetour.TabIndex = 135
+        Me.BtnRetour.Text = "Retour"
+        Me.BtnRetour.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(343, 272)
+        Me.CheckBox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(18, 17)
+        Me.CheckBox3.TabIndex = 133
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(343, 149)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(18, 17)
+        Me.CheckBox1.TabIndex = 131
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(17, 266)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(125, 28)
+        Me.Button3.TabIndex = 128
+        Me.Button3.Text = "Porte clés"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.ComboBox3.Location = New System.Drawing.Point(258, 269)
+        Me.ComboBox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(65, 24)
+        Me.ComboBox3.TabIndex = 127
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(17, 139)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(125, 42)
+        Me.Button1.TabIndex = 124
+        Me.Button1.Text = "Gobelet en plastique"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.ComboBox1.Location = New System.Drawing.Point(258, 145)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(65, 24)
+        Me.ComboBox1.TabIndex = 123
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.White
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.Label5.Location = New System.Drawing.Point(275, 64)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(98, 39)
+        Me.Label5.TabIndex = 122
+        Me.Label5.Text = "Autre"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.DarkRed
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.Label2)
+        Me.Panel2.Location = New System.Drawing.Point(2, 449)
+        Me.Panel2.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(379, 57)
+        Me.Panel2.TabIndex = 121
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label4.Location = New System.Drawing.Point(308, 7)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(36, 39)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "‎€"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label3.Location = New System.Drawing.Point(197, 17)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(65, 29)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "0000"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Label2.Location = New System.Drawing.Point(5, 7)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(136, 39)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "TOTAL:"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(2, -3)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(4)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(379, 64)
+        Me.Panel1.TabIndex = 120
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(4, 10)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(294, 51)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nouvelle Liste"
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 16
+        Me.ListBox3.Items.AddRange(New Object() {"Poids : 10g piéce", "Marque : The portman", "Pays : Allemagne", "Bio : NON"})
+        Me.ListBox3.Location = New System.Drawing.Point(19, 277)
+        Me.ListBox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(124, 68)
+        Me.ListBox3.TabIndex = 144
+        Me.ListBox3.Visible = False
+        '
+        'EcoChoisirAutre
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(382, 503)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.LblPrixPorteClef)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.LblPrixGob)
+        Me.Controls.Add(Me.BtnRetour)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "EcoChoisirAutre"
         Me.Text = "EcoChoisirAutre"
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents Label7 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents LblPrixPorteClef As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents LblPrixGob As Label
+    Friend WithEvents BtnRetour As Button
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox3 As ListBox
 End Class
--- Application IHM/Application IHM/NouvelleListe.vb
+++ Application IHM/Application IHM/NouvelleListe.vb
@@ -21,6 +21,8 @@
         ElseIf Form1.Label2.Text = "Economique" Then
             IsEco.Text = "Economique"
             EcoChoisirViande.Show()
+
+            EcoChoisirViande.Label1.Text = BoxNewList.Text
         End If
 
 
@@ -39,6 +41,8 @@
             IsEco.Text = "Economique"
             EcoSurgele.Show()
 
+            EcoSurgele.Label1.Text = BoxNewList.Text
+
         End If
 
     End Sub
@@ -54,6 +58,8 @@
         ElseIf Form1.Label2.Text = "Economique" Then
             IsEco.Text = "Economique"
             EcoFruitLegumes.Show()
+
+            EcoFruitLegumes.Label1.Text = BoxNewList.Text
         End If
 
     End Sub
@@ -68,6 +74,9 @@
 
         ElseIf Form1.Label2.Text = "Economique" Then
             IsEco.Text = "Economique"
+            EcoEpice.Show()
+
+            EcoEpice.Label1.Text = BoxNewList.Text
         End If
 
     End Sub
@@ -82,6 +91,9 @@
 
         ElseIf Form1.Label2.Text = "Economique" Then
             IsEco.Text = "Economique"
+            EcoProduitDuMonde.Show()
+
+            EcoProduitDuMonde.Label1.Text = BoxNewList.Text
         End If
 
     End Sub
@@ -103,22 +115,6 @@
         MsgBox("Votre Liste a ete sauvegarder")
     End Sub
 
-    Private Sub BoxtxtNew_TextChanged(sender As Object, e As EventArgs) Handles BoxNewList.TextChanged
-
-    End Sub
-
-    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
-        ChoisirAutre.Show()
-    End Sub
-
-    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
-
-    End Sub
-
-    Private Sub NouvelleListe_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
     Private Sub NouvelleListe_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
         'Verifie si la liste a bien ete enrengistrer avant de fermer la fenetre
         If Not btnSaveClicked Then
@@ -129,5 +125,22 @@
     End Sub
     Private Sub Button3_Click_1(sender As Object, e As EventArgs) Handles Button3.Click
         FormScann.Show()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        'Verifie si l'option eco est activer
+        If Form1.Label2.Text = "" Then
+            IsEco.Text = ""
+            ChoisirAutre.Show()
+            'modifier le label de Autre a la valeur du text input
+            ChoisirAutre.Label1.Text = BoxNewList.Text
+
+        ElseIf Form1.Label2.Text = "Economique" Then
+            IsEco.Text = "Economique"
+            EcoChoisirAutre.Show()
+
+            'modifier le label de Autre a la valeur du text input
+            EcoChoisirAutre.Label1.Text = BoxNewList.Text
+        End If
     End Sub
 End Class
